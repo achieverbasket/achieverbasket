a30cca3a5ae105d8c19adedfcf4bab0f3b28ff24
patoneesvn <patoneesvn@982e0730-df8a-4b79-8190-77509d8913f0>
2017-12-28 11:46:54 +0000

    adding method to get multiple templates

--- trunk/src/main/java/com/ab/dao/CertificateTemplateDao.java
+++ trunk/src/main/java/com/ab/dao/CertificateTemplateDao.java
@@ -1,5 +1,8 @@
 package com.ab.dao;
 
+import java.util.List;
+
+import com.ab.type.CertificateType;
 import com.ab.vo.certificate.CertificateTemplate;
 
 public interface CertificateTemplateDao {
@@ -9,6 +12,8 @@ public interface CertificateTemplateDao {
 	void updateCertificateTemplate(CertificateTemplate certificateTemplate);
 
 	CertificateTemplate getCertificateTemplate(Long certificateTemplateId);
+	
+	List<CertificateTemplate> getCertificateTemplateList(Long issuerId, CertificateType certificateType);
 
 	void removeCertificateTemplate(Long certificateTemplateId);
 }
--- trunk/src/main/java/com/ab/dao/impl/CertificateTemplateDaoImpl.java
+++ trunk/src/main/java/com/ab/dao/impl/CertificateTemplateDaoImpl.java
@@ -1,12 +1,16 @@
 package com.ab.dao.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.ab.dao.CertificateTemplateDao;
 import com.ab.type.CertificateType;
+import com.ab.vo.certificate.Certificate;
 import com.ab.vo.certificate.CertificateTemplate;
 
 @Repository
@@ -46,6 +50,23 @@ public class CertificateTemplateDaoImpl implements CertificateTemplateDao {
 				return template;
 			});
 	}
+	
+	@Override
+	public List<CertificateTemplate> getCertificateTemplateList(Long issuerId, CertificateType certificateType) {
+		String sql = "SELECT TEMPLATE_NAME, CERTIFICATE_TEMPLATE_ID, CERTIFICATE_TYPE_ID, CERTIFICATE_DESC, CREATED_BY, CREATED_TIME, MODIFIED_BY, MODIFIED_TIME FROM CERTIFICATE_TEMPLATE WHERE ISSUER_ID=? AND CERTIFICATE_TYPE_ID=?";
+		
+		return jdbcTemplate.query(sql, new Object[] {issuerId,  certificateType.getCertificateTypeId()}, (RowMapper<CertificateTemplate>) (rs,arg) -> {
+				rs.next();
+				CertificateTemplate template = new CertificateTemplate();
+				template.setCertificateTemplateId(rs.getLong("CERTIFICATE_TEMPLATE_ID"));
+				template.setTemplateName(rs.getString("TEMPLATE_NAME"));
+				template.setIssuerId(issuerId);
+				template.setCertificateDesc(rs.getString("CERTIFICATE_DESC"));
+				return template;
+			});
+
+	}
+
 
 	@Override
 	public void removeCertificateTemplate(Long certificateTemplateId) {
