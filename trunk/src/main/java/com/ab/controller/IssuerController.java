36b1291b096be9b4fc136121bbffe4e0cf86fe87
patoneesvn <patoneesvn@982e0730-df8a-4b79-8190-77509d8913f0>
2018-01-01 04:38:32 +0000

    adding process flow

--- trunk/src/main/java/com/ab/controller/IssuerController.java
+++ trunk/src/main/java/com/ab/controller/IssuerController.java
@@ -1,14 +1,25 @@
 package com.ab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ab.constant.config.ApplicationPageConstant;
+import com.ab.service.CertificateService;
+import com.ab.service.CertificateTemplateService;
+import com.ab.type.CertificateType;
+import com.ab.vo.certificate.Certificate;
 
 @Controller
 @RequestMapping("/issuer")
 public class IssuerController {
+	
+	@Autowired
+	CertificateService certificateService;
+	
+	@Autowired
+	CertificateTemplateService certificateTemplateService;
 	
 	// get issue certifiate page
 	@RequestMapping(path="/certificate/create" ,method=RequestMethod.GET)
@@ -20,21 +31,32 @@ public class IssuerController {
 	
 	//fetch list of templates available in repository to issuer to choose from
 	@RequestMapping(path="/certificate/templates" ,method=RequestMethod.GET)
-	public String getCertificateTemplates()
+	public String getAvailableCertificateTemplates(CertificateType certificateType)
 	{
-		System.out.println("in getCertificateTemplates");
+		System.out.println("in getAvailableCertificateTemplates");
+		certificateTemplateService.getCertificateTemplateList(null, certificateType);
+		return ApplicationPageConstant.certificatetemplates_page;
+	}
+	
+	//fetch list of templates assigned to given issuer
+	@RequestMapping(path="/certificate/templates" ,method=RequestMethod.GET)
+	public String getIssuerCertificateTemplates(Long issuerId, CertificateType certificateType)
+	{
+		System.out.println("in getIssuerCertificateTemplates");
+		certificateTemplateService.getCertificateTemplateList(issuerId, certificateType);
 		return ApplicationPageConstant.certificatetemplates_page;
 	}
 	
 	// get issue certifiate page
 	@RequestMapping(path="/certificate/issue" ,method=RequestMethod.GET)
-	public String issueCertificateToCandidate()
+	public String issueCertificateToCandidate(Long issuerId, Long candidateId, Certificate certificate)
 	{
 		System.out.println("in issueCertificateToCandidate");
+		certificate.setCandidateId(candidateId);
+		certificate.setIssuerId(issuerId);
+		certificateService.saveCertificate(certificate);
 		return ApplicationPageConstant.issuecertificate_page;
 	}
-	
-	
 	
 	//search certificate
 	@RequestMapping(path="/certificate/search" ,method=RequestMethod.GET)
@@ -53,10 +75,10 @@ public class IssuerController {
 	}
 	
 	// get issue certifiate page
-		@RequestMapping(path="/certificate/loadimage" ,method=RequestMethod.GET)
-		public String loadCertificateImage()
-		{
-			System.out.println("in loadCertificateImage");
-			return ApplicationPageConstant.loadcertificateimage_page;
-		}
+	@RequestMapping(path="/certificate/loadimage" ,method=RequestMethod.GET)
+	public String loadCertificateImage()
+	{
+		System.out.println("in loadCertificateImage");
+		return ApplicationPageConstant.loadcertificateimage_page;
+	}
 }
