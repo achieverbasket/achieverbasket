722f930c62a2a54eba36946326ca8eff894df887
patoneesvn <patoneesvn@982e0730-df8a-4b79-8190-77509d8913f0>
2018-01-01 04:41:31 +0000

    Adding Certificate Template Service

---
+++ src/main/java/com/ab/service/impl/CertificateTemplateServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ab.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ab.dao.CertificateTemplateDao;
+import com.ab.service.CertificateTemplateService;
+import com.ab.type.CertificateType;
+import com.ab.vo.certificate.CertificateTemplate;
+
+public class CertificateTemplateServiceImpl implements CertificateTemplateService {
+	
+	@Autowired
+	CertificateTemplateDao certificateTemplateDao;
+	
+	public CertificateTemplate saveCertificateTemplate(CertificateTemplate certificateTemplate) {
+		return certificateTemplateDao.saveCertificateTemplate(certificateTemplate);
+	}
+	
+	public void updateCertificateTemplate(CertificateTemplate certificateTemplate) {
+		certificateTemplateDao.updateCertificateTemplate(certificateTemplate);
+	}
+
+	public CertificateTemplate getCertificateTemplate(Long certificateTemplateId) {
+		return certificateTemplateDao.getCertificateTemplate(certificateTemplateId);
+	}
+	
+	public List<CertificateTemplate> getCertificateTemplateList(Long issuerId, CertificateType certificateType) {
+		
+		if(issuerId==null) {
+			issuerId = 99999l;//Default issuer Id for available templates to everyone
+		}
+		return certificateTemplateDao.getCertificateTemplateList(issuerId, certificateType);
+	}
+
+	public void removeCertificateTemplate(Long certificateTemplateId) {
+		certificateTemplateDao.removeCertificateTemplate(certificateTemplateId);
+	}
+
+}
